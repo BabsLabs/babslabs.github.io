6d39cd5183055365be6853e3ba54ec89f9ffe111
Brian Bower <brian@brianbower.com>
2020-06-02 14:36:09 -0600

    Updates

---
+++ precache-manifest.63d9e0564b6f2433bc3453bae87a846a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45886f4663be7f490f51d5f35e8fd099",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e62ea3f4f3f2a2c5e6f",
+    "url": "/static/css/main.6456240a.chunk.css"
+  },
+  {
+    "revision": "0211a94cd8d192659672",
+    "url": "/static/js/2.819565f3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.819565f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e62ea3f4f3f2a2c5e6f",
+    "url": "/static/js/main.90434110.chunk.js"
+  },
+  {
+    "revision": "046c7e7a4c8c136663a1",
+    "url": "/static/js/runtime-main.0595ee31.js"
+  },
+  {
+    "revision": "b5b88b5875d5ab5dfd8764855868eb86",
+    "url": "/static/media/babslabs-monkey-logo-portfolio.b5b88b58.png"
+  }
+]);
